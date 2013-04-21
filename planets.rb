f83a0759736abcd70fc01feb231f4961ddc90497
Micah Geisel <micah@botandrose.com>
2013-04-21 16:11:14 -0700

    use ruby conventions for variable naming.

--- planets.rb
+++ planets.rb
@@ -1,19 +1,19 @@
-#Programmin' da planets, mothah fuckah
-#By Paul Ogden
+# Programmin' da planets, mothah fuckah
+# By Paul Ogden
 
 # Attempting to write a program that takes the current time and calculated the position of jupiter and prints the coordinates
 
-currentTime = Time.new
-benchmarkTime = Time.utc(2000,"jan",1,00,00,00)
-differenceTime = currentTime-benchmarkTime
+current_time = Time.new
+benchmark_time = Time.utc(2000, "jan", 1, 00, 00, 00)
+difference_time = current_time - benchmark_time
 # Assuming that at 2000-01-01 00:00:00 Jupiter was at 0 degrees and 4.95AU
 
 # Jupiter orbits the sun every 11.86 earth years or once every 4,332 days
 
 # 4.95 AU at it's closest point (perihelion) and 5.46 AU at it's furthest point (aphelion)
-distanceFromSun = 5.205
+distance_from_sun = 5.205
 
 # Angular speed = 1.68x10^-8 rad/s or 9.63x10^-7 deg/s
-angleFromSun = differenceTime * 374284800
+angle_from_sun = difference_time * 374284800
 
-print "At #{currentTime} the distance of the planet Jupiter from the sun is #{distanceFromSun} AU and it is at #{angleFromSun} degrees angle from the sun"
+print "At #{current_time} the distance of the planet Jupiter from the sun is #{distance_from_sun} AU and it is at #{angle_from_sun} degrees angle from the sun"
